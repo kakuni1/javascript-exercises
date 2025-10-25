acbf737d2c355262800db9f58f89e3b35e816905
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2025-10-25 12:22:04 -0700

    lesson: 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let str = "",
+    strCopy = "";
+  for (let i = 0; i < num; i++) {
+    str = strCopy.concat(str, string);
+  }
+  if (num >= 0) {
+    return str;
+  } else {
+    return (str = "ERROR");
+  }
 };
 
 // Do not edit below this line

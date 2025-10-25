2685df58f493ee44441030808a854c89fffdf46f
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2025-10-25 16:57:12 -0700

    lesson: 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (text) {
+  let reverseText = "";
+  for (let i = text.length - 1; i >= 0; i--) {
+    tempChar = text.charAt(i);
+    reverseText += tempChar;
+  }
+  return reverseText;
 };
 
 // Do not edit below this line

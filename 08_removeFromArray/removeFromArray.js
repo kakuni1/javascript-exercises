1c67ea79fca6cbfd869eba2379ca80947e37e407
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2025-10-25 23:13:42 -0700

    lesson: 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...num) {
+  for (let i = 0; i < num.length; i++) {
+    tempArray = array.filter((number) => number !== num[i]);
+    array = tempArray;
+  }
+  return array;
 };
 
 // Do not edit below this line

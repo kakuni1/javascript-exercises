5910a2417856bb0567abc337d22f6c59cf12bde0
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2025-11-13 10:36:23 -0800

    lesson: 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const divFour = year % 4 === 0;
+  const divHundred = year % 100 === 0;
+  const divFourHundred = year % 400 === 0;
 
+  if (divFour && divHundred && divFourHundred) return true;
+  else if (divFour && divHundred) return false;
+  else if (divFour) return true;
+  else return false;
 };
 
 // Do not edit below this line

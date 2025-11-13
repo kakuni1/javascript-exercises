85de76ab810407c78fc679d31a413f3ca76e369f
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2025-11-12 19:52:01 -0800

    lesson: 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  // a=2, b=4
+  let add = 0;
 
+  if (Number.isInteger(a) && a > 0 && Number.isInteger(b) && b > 0) {
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+      add += i;
+    }
+    return add;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line

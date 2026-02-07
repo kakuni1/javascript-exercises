7862a8cf01fb6fd3996020ef6f11122c6a834a43
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-02-07 11:27:42 -0800

    lesson: 12_caclulator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return (n = a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return (n = a - b);
 };
 
-const sum = function() {
-	
+const sum = function (n) {
+  return n.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (n) {
+  return n.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return (n = a ** b);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0 || n === 1) return 1;
+  for (let i = n - 1; i >= 1; i--) {
+    n *= i;
+  }
+  return n;
 };
 
 // Do not edit below this line
@@ -29,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };

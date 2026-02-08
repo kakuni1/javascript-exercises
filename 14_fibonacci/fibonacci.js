add01d4b2d768b636f31e2f71d8a9e9fa34417d5
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-02-07 18:48:14 -0800

    lesson: 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  array = [1, 1];
 
+  if (typeof n === "string") n = parseInt(n);
+
+  switch (true) {
+    case n === 0:
+      return 0;
+    case n < 0:
+      return "OOPS";
+    default:
+      for (i = 0; i < n - 2; i++) {
+        array.push(array[i] + array[i + 1]);
+      }
+
+      return array[n - 1];
+  }
 };
 
 // Do not edit below this line

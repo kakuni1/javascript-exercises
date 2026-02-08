2652ac6f8ac92c695fc1cad0475ce4cd8df7a13c
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-02-07 17:08:15 -0800

    lesson: 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+  let reverseString = "";
+  cleanString = inputString.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
+  for (n = cleanString.length - 1; n >= 0; n--)
+    reverseString = reverseString.concat(cleanString[n]);
+  return cleanString === reverseString;
 };
 
 // Do not edit below this line

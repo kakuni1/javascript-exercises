3810af7bb587d7ad341017298ec96ffcc1e06b64
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-02-08 16:03:49 -0800

    lesson: 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const getAge = function (birthYear, deathYear) {
+  if (!deathYear) deathYear = new Date().getFullYear();
+  return deathYear - birthYear;
+};
 
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    const oldestAge = getAge(
+      oldestPerson.yearOfBirth,
+      oldestPerson.yearOfDeath,
+    );
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath,
+    );
+    console.log(oldestAge);
+    return oldestAge < currentAge ? currentPerson : oldestPerson;
+  });
 };
 
 // Do not edit below this line

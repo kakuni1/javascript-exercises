a8d379381c0d1c74f1864cbc118dcb4f0373d38f
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-07-21 22:12:49 -0700

    contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,4 +1,13 @@
-const contains = () => {};
+const contains = (data, input) => {
+  if (data === input || (Number.isNaN(input) && Number.isNaN(data)))
+    return true;
+  if (typeof data === "object" && data !== null)
+    for (let key in data) {
+      const result = contains(data[key], input);
+      if (result) return true;
+    }
+  return false;
+};
 
 // Do not edit below this line
 module.exports = contains;

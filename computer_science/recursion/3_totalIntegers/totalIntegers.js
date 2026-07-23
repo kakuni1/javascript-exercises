f1be18cb164628ea3d7476502899906e3557c2db
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-07-23 00:38:13 -0700

    totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,4 +1,22 @@
-const totalIntegers = () => {};
+const totalIntegers = (data, result = []) => {
+  if (typeof data !== "object" || data === null) return undefined;
+
+  const array = Array.isArray(data)
+    ? Object.values(
+        data.flat(Infinity).filter((a) => a !== undefined && a !== null),
+      )
+    : Object.values(data);
+
+  for (let key in array) {
+    if (typeof array[key] === "object" && array[key] !== null) {
+      console.log(array[key]);
+      totalIntegers(array[key], result);
+    }
+    if (typeof array[key] !== "string" && Number.isInteger(array[key]) === true)
+      result.push(array[key]);
+  }
+  return result.length;
+};
 
 // Do not edit below this line
 module.exports = totalIntegers;
